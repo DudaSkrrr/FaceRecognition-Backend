fafa6d35ad5b3ccdb14deb3d2d787cd0f677f5ff
DudaSkrrr <151225621+DudaSkrrr@users.noreply.github.com>
2024-02-14 21:24:55 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ const knex = require('knex')
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      connectionString : process.env.DATABASE_URL,
+      ssl: {rejectUnautorized: false},
+      host : process.env.DATABASE_HOST,
       port : 5432,
-      user : 'postgres',
-      password : 'test',
-      database : 'smart-brain'
+      user : process.env.DATABASE_USER,
+      password : process.env.DATABASE_PW,
+      database : process.env.DATABASE_DB
     }
   });
 
